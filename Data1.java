91c0d30582ca6a23f6bae71071822b3b938be7a2
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2023-09-11 20:19:47 -0300

    Atualizar o teste1.java

--- Data1.java
+++ Data1.java
@@ -1,7 +1,29 @@
-package br.edu.ifsp.exercicio.data;
 
-public class Data1{
-   public Data1() {
-	   
-   }
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        System.out.println("Digite o terceiro número: ");
+        double numero3 = scanner.nextDouble();
+
+        double media = calcularMedia(numero1, numero2, numero3);
+
+        System.out.println("A média dos números é: " + media);
+
+        scanner.close();
+    }
+
+    public static double calcularMedia(double num1, double num2, double num3) {
+        return (num1 + num2 + num3) / 3;
+    }
 }
+
